a01a2483c4e6c226edd7cd126e4095b41b85bd8e
Matthew Cserhati <csmatyi1@gmail.com>
2025-08-07 12:26:43 -0700

    Update molecular_baraminology_pipeline.R

--- molecular_baraminology_pipeline.R
+++ molecular_baraminology_pipeline.R
@@ -7,14 +7,15 @@ library(stringr)
 library(ggplot2)
 library(cluster)
 library(factoextra)
+library(diptest)
+library(e1071)
 
 setwd("C:\\Users\\csmat\\OneDrive\\Desktop\\CreationScience\\Talks\\CRSconferences\\CRS2025\\workshop\\Elephants")
 
 # Define list of Latin species names
-# species_list <- c("Homo sapiens", "Mus musculus", "Canis lupus")
-# species_list <- c("Lilium davidii", "Notholirion campanulatum", "Fritillaria sinica")
 species_list <- read.table(choose.files(filters = Filters[c("txt"),]),sep="\t",check.names=FALSE)
 species_list <- species_list$V1
+n_species <- length(species_list)
 
 # Search for:
 # 1. organelle genome
@@ -22,10 +23,10 @@ species_list <- species_list$V1
 #    b. clp genome (MAFFT)
 # 2. protein (clustalw)
 
-mito <- FALSE
-clp <- TRUE
+mito <- TRUE
+clp <- FALSE
 prot <- FALSE
-protein_name <- "TP53" # "NULL"
+protein_name <- NULL
 
 # Initialize empty list to hold sequences
 sequence_list <- list()
@@ -81,12 +82,13 @@ if (prot == FALSE) {
 }
 
 # Run alignment using msa
+# method: select from ClustalW, ClustalOmega, or Muscle
 if (mito == TRUE) {
-  alignment <- msaClustalW(string_set)
+  alignment <- msa(string_set, method = "ClustalW")
+} else if (prot == TRUE) {
+  alignment <- msa(string_set, method = "ClustalOmega")
 } else if (clp == TRUE) {
   alignment <- msa(string_set, method = "Muscle")
-} else if (prot == TRUE) {
-  alignment <- msaClustalW(string_set)
 }
 
 # Convert to character vector of aligned sequences
@@ -106,22 +108,39 @@ class(seqinr_alignment) <- "alignment"
 idmx <- dist.alignment(seqinr_alignment,"identity")
 mx <- 1 - as.matrix(idmx,nrow=n_species,ncol=n_species)
 
+# Calculate Hopkins statistic
+result <- get_clust_tendency(mx, n = nrow(mx)-1, graph = FALSE)
+print(result$hopkins_stat)
 
 ######################## Plots ###########################
 
 message("Drawing plots...")
 
-# Betweenness and withinness plots
+# distance matrix
+dmx <- 1 - mx
 
-### 1. Silhouette plot
-jpeg("Silhouette.jpg")
-fviz_nbclust(mx, kmeans, method = "silhouette", k.max=20) + theme_classic() # pam|kmeans|hcut|clara
-dev.off()
-
-### 2. Elbow plot
+### 1. Elbow plot
 jpeg("Elbow.jpg")
-fviz_nbclust(mx, kmeans, method = "wss", k.max=20) + theme_classic() # kmeans|pam (kmeans)
+ep <- fviz_nbclust(dmx, kmeans, method = "wss", k.max=20) + theme_classic() # kmeans|pam (kmeans)
 dev.off()
+# save elbow plot values
+elbow <- ep$data
+colnames(elbow)[2] <- 'twss'
+elbow$diff <- c(NA,-diff(elbow$twss))/elbow$twss
+elbow$diff <- sprintf("%.3f",elbow$diff)
+elbow$twss <- sprintf("%.3f",elbow$twss)
+write.table(elbow,'elbow_stats.txt',quote=F,row.names = F,sep="\t")
+
+### 2. Silhouette plot
+
+kmeans_result <- kmeans(dmx, centers = 6)
+silhouette_info <- silhouette(kmeans_result$cluster, dmx)
+sil <- factoextra::fviz_silhouette(silhouette_info)
+jpeg("Silhouette.jpg")
+sil
+dev.off()
+sil$data$sil_width <- sprintf("%.3f",sil$data$sil_width)
+write.table(sil$data,"silhouette_stats.txt",quote=F,sep="\t",row.names=F)
 
 ### 3. Heatmap
 species <- row.names(mx)
@@ -142,7 +161,7 @@ clr = gray.colors(100) # grayscale
 
 # n is the estimated number of clusters/kinds/baramins
 # if you change the estimate, restart from this point!
-n <- 5
+n <- 6
 
 # clustering methods: ward.D ward.D2 single median average mcquitty complete centroid
 clusmeth="ward.D2" 
@@ -154,55 +173,75 @@ clus_clrs_vec <- clus_clrs[ctk[species[sort(row.clusters$order)]]]
 
 ### Heatmap code ###
 # provide a name for the heatmap
-heatmap_name=paste("heatmap_",clusmeth,"_",n,"_250724.jpg",sep="")
-jpeg(filename = heatmap_name, height = 3000, width = 3000, units = "px", res=400) # topo.colors(100) 5500, 5000
-h <- heatmap(mx_hm, symkey=F, symbreaks=F, scale="none", dendrogram=F, Rowv=T, Colv=T, col = clr, RowSideColors=clus_clrs_vec, breaks = myBreaks, border_color=NA, na.color="white", margin = c(15,15),
-             cexRow=cexx,cexCol=ceyy, key=T, trace="none", lmat=rbind( c(4, 3), c(2,1), c(0,0) ), lhei=c(1.8,6.5,1), hclustfun = function(d) hclust(d, method=clusmeth), # dendrogram="none",
-             labCol=as.expression(lapply(colnames(mx), function(a) bquote(italic(.(a))))),labRow=as.expression(lapply(rownames(mx), function(a) bquote(italic(.(a))))))
+myDate <- format(Sys.Date(), "%d%m%Y")
+heatmap_name=paste("heatmap_",clusmeth,"_k",n,"_",myDate,".jpg",sep="")
+jpeg(filename = heatmap_name, height = 3000, width = 3000, units = "px", res = 400)
+h <- heatmap(mx_hm, 
+             symkey=F, 
+             symbreaks=F, 
+             scale="none", 
+             dendrogram=F, 
+             Rowv=T, 
+             Colv=T, 
+             col=clr, 
+             RowSideColors=clus_clrs_vec, 
+             breaks=myBreaks, 
+             border_color=NA, 
+             na.color="white", 
+             margin = c(15,15),
+             cexRow=cexx,
+             cexCol=ceyy, 
+             key=T, 
+             trace="none", 
+             lmat=rbind(c(4, 3), c(2, 1)),
+             lhei=c(1.8,6.5), 
+             hclustfun=function(d) hclust(d, method=clusmeth),
+             labCol=as.expression(lapply(colnames(mx), function(a) bquote(italic(.(a))))),
+             labRow=as.expression(lapply(rownames(mx), function(a) bquote(italic(.(a)))))
+              )
 invisible(dev.off())
 
 ######################### Stats files #########################
 
 # stats and clusters files
-# number of clusters
-n <- 7
-
-row.clusters = hclust(dist(mx),method=clusmeth)
-ctk <- cutree(row.clusters,k=n)
-
 # clusters from ctk
-write.table(ctk, file="kclusters.txt", col.names=F, quote=F, sep="\t")
+write.table(ctk, file="cluster_membership.txt", col.names=F, quote=F, sep="\t")
 
-header = "cluster\tspecies\tmin\tmean\tmax\tSEM\tp-value\tneglog"
-write(header, file="kstats.txt", sep="\t", append=T)
+header = "cluster\tspecies\tmin\tmean\tmax\tSEM\tp-value\tneglog\tDip\tDip p-val\tSkew"
+write(header, file="cluster_stats.txt", sep="\t", append=T)
 
 cluster_sizes = as.vector(table(ctk))
 for (n_cluster in 1:n) {
-  csize = cluster_sizes[n_cluster]
+  csize <- cluster_sizes[n_cluster]
   if (csize >= 3) {
-    m1 = as.matrix(mx[ctk == n_cluster,ctk == n_cluster])
+    m1 <- as.matrix(mx[ctk == n_cluster,ctk == n_cluster])
+    x <- m1[upper.tri(m1)]
+
+    m2 <- as.matrix(cbind(mx[ctk != n_cluster,ctk == n_cluster],t(mx[ctk == n_cluster,ctk != n_cluster])))
+    m2b <- m2[!duplicated(colnames(m2))]
     
-    x = m1[upper.tri(m1)]
-    ll = dim(m1)[1]
+    t <- t.test(x,m2b)
+    pval <- t$p.value
+    nglog <- -log10(pval)
+    min <- min(x)
+    max <- max(x)
+    dip <- dip.test(x)
+    dip_d <- dip$statistic
+    dip_p <- dip$p.value
+    skew <- skewness(x)
     
-    m2 = as.matrix(cbind(mx[ctk != n_cluster,ctk == n_cluster],t(mx[ctk == n_cluster,ctk != n_cluster])))
-    m2b = m2[!duplicated(colnames(m2))]
+    mean2 <- sprintf("%.3f", mean(x))
+    sem2 <- sprintf("%.3f", sd(x)/sqrt(csize))
+    min2 <- sprintf("%.3f", min)
+    max2 <- sprintf("%.3f", max)
+    pval2 <- sprintf("%.3f", pval)
+    nglog2 <- sprintf("%.3f", nglog)
+    dip_d2 <- sprintf("%.3f", dip_d)
+    dip_p2 <- sprintf("%.3f", dip_p)
+    skew2 <- sprintf("%.3f", skew)
     
-    t = t.test(x,m2b)
-    pval = t$p.value
-    nglog = -log10(pval)
-    min = min(x)
-    max = max(x)
-    
-    mean2 = sprintf("%.3f", mean(x))
-    sem2 = sprintf("%.3f", sd(x)/sqrt(csize))
-    min2 = sprintf("%.3f", min)
-    max2 = sprintf("%.3f", max)
-    pval2 = sprintf("%.3f", pval)
-    nglog2 = sprintf("%.3f", nglog)
-    
-    stats = paste(n_cluster, ll, min2, mean2, max2, sem2, pval, nglog2, sep="\t")
-    stats2 = gsub("\n","\t",stats)
-    write(stats, file="kstats.txt", sep="\t", append = T)
+    stats <- paste(n_cluster, csize, min2, mean2, max2, sem2, pval, nglog2, dip_d2, dip_p2, skew2, sep="\t")
+    stats2 <- gsub("\n","\t",stats)
+    write(stats, file="cluster_stats.txt", sep="\t", append = T)
   }
 }
